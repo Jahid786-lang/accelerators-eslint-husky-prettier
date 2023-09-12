66ff3c22239f6b4df6d13d3390827c3096de6ec9
jahid.cosmostaker@gmail.com <jahid.cosmostaker@gmail.com>
2023-09-12 15:42:57 +0530

    add bin file

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const { lint, format } = require('./your-accelerator-scripts');
+
+program
+  .version('1.0.0')
+  .description('A CLI for your React accelerator')
+  .option('-l, --lint', 'Run linting')
+  .option('-f, --format', 'Run code formatting')
+  .parse(process.argv);
+
+if (program.lint) {
+  lint();
+}
+
+if (program.format) {
+  format();
+}
